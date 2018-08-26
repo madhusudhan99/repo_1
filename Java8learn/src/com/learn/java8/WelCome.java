70d1020de97353ff093dc3cba9460b3ce019006c
madhu <email2madhu.poranki@gmail.com>
2018-08-26 13:27:23 +0530

    Spring web project

--- Java8learn/src/com/learn/java8/WelCome.java
+++
@@ -1,7 +0,0 @@
-package com.learn.java8;
-
-public class WelCome {
-public static void main(String[] args) {
-	System.out.println("WelCome TO Java8 ");
-}
-}
--- learnjava/src/learnjava/Test.java
+++
@@ -1,7 +0,0 @@
-package learnjava;
-
-public class Test {
-public static void main(String[] args) {
-	System.out.println("Wel Come To Java8");
-}
-}
